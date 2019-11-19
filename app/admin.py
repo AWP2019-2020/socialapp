057a4dc9a4d644f3e09b4250668e18f0fc064eca
dianaboiangiu <boiangiudiana58@gmail.com>
2019-11-19 14:50:18 +0200

    Add Comment and Country models

--- app/admin.py
+++ app/admin.py
@@ -2,9 +2,20 @@ from django.contrib import admin
 from . import models
 # Register your models here.
 
+class CountryAdmin(admin.ModelAdmin):
+        list_display = ('code', 'name')
+        search_fields = ('name',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+        list_display = ('text', 'created_by', 'post')
+
+
 class UserProfileAdmin(admin.ModelAdmin):
         list_display =('user', 'birthday')
         search_fields=('user__username',)
 
+admin.site.register(models.Country, CountryAdmin)
+admin.site.register(models.Comment, CommentAdmin)
 admin.site.register(models.Post)
-admin.site.register(models.UserProfile, UserProfileAdmin) 
+admin.site.register(models.UserProfile, UserProfileAdmin)
--- app/models.py
+++ app/models.py
@@ -13,8 +13,31 @@ class Post(models.Model):
     def __str__(self):
         return "{} created by {} at {}".format(self.text, self.created_by.username, self.created_at)
 
+
+class Country(models.Model):
+    code = models.CharField(max_length=3, primary_key=True)
+    name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
+
 class UserProfile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
     birthday = models.DateField(blank=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name="profiles", blank=True, null=True)
     friend_requests = models.ManyToManyField(User, related_name='friend_requests', blank=True)
     friends = models.ManyToManyField(User, related_name='friends', blank=True)
+
+class Comment(models.Model):
+    text = models.CharField(max_length=200)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{} created by {} at {}".format(self.text, self.created_by.username, self.created_at)
