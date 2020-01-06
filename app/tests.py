54f3daf8653f4dc698f95d770ca84c42fa73dfdc
Alexandru Neamtu <alexneamtu@gmail.com>
2020-01-06 12:44:29 +0200

    Curs 10 - Added post_detail test

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from app.models import Comment, Post, User
+
+
+class PostTestCase(TestCase):
+    def test_post_detail(self):
+        user = User.objects.create_user(username="testuser", password="testpassword")
+
+        post = Post.objects.create(text="Test Post", created_by_id=user.pk)
+        comment = Comment.objects.create(text="Test Comment", post=post, created_by_id=user.pk)
+
+        response = self.client.get(reverse('post_detail', args=[post.pk]))
+
+        self.assertEquals(response.context['post'], post)
+        self.assertEquals(len(response.context['post'].comments.all()), 1)
+        self.assertEquals(response.context['post'].comments.first(), comment)
